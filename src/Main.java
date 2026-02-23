46f9f02e8bd4280d5350294aa2c12725b72aaaf3
Алёна <146723120+asdalaaa@users.noreply.github.com>
2026-02-24 00:48:22 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("приветикПривет!");
+        System.out.println("приветик");
         System.out.println("Новый текст");
     }
 }
